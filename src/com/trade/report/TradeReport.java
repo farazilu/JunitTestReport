aaf019cc935a6cbeb1c7939dce966f26bfc25f65
faraz <faraz@adeogroup.co.uk>
2018-02-07 17:02:32 +0000

    start processing instructions

--- src/com/trade/report/TradeReport.java
+++ src/com/trade/report/TradeReport.java
@@ -1,15 +1,33 @@
 package com.trade.report;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TradeReport {
 
-	
+	private Map<String, Double> incomming;
+	private Map<String, Double> outgoing;
+
+	TradeReport() {
+		incomming = new HashMap<String, Double>();
+		outgoing = new HashMap<String, Double>();
+	}
+
 	void addInstruction(Instruction instruction) {
 		if (instruction.is_sattled()) {
-			// process report
-			
-			//assosiative array in java
-			//https://stackoverflow.com/questions/5122913/java-associative-array
-			//https://docs.oracle.com/javase/tutorial/collections/interfaces/map.html
+			// process report as instruction arrive
+			String settlementDate = instruction.getSettlementDate();
+			Double trade = instruction.getTrade();
+			if (instruction.getInstructionType() == 'S') {
+				Double freq = incomming.get(settlementDate);
+				incomming.put(settlementDate, (freq == null) ? trade : freq + trade);
+			} else {
+				Double freq = outgoing.get(settlementDate);
+				outgoing.put(settlementDate, (freq == null) ? trade : freq + trade);
+			}
+			// associative array in java
+			// https://stackoverflow.com/questions/5122913/java-associative-array
+			// https://docs.oracle.com/javase/tutorial/collections/interfaces/map.html
 		}
 	}
 
