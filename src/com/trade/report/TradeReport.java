2faa4de64951b4723375a7195bc23f3d7c0dde63
faraz <faraz@adeogroup.co.uk>
2018-02-07 16:42:36 +0000

    add map tutorial for refrence
    Signed-off-by: faraz <faraz@adeogroup.co.uk>

--- src/com/trade/report/TradeReport.java
+++ src/com/trade/report/TradeReport.java
@@ -9,6 +9,7 @@ public class TradeReport {
 			
 			//assosiative array in java
 			//https://stackoverflow.com/questions/5122913/java-associative-array
+			//https://docs.oracle.com/javase/tutorial/collections/interfaces/map.html
 		}
 	}
 
